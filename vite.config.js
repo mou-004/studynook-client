e05a8ad7a5f7639ab6ca3bc1ae8e8b94ae37c766
Halima Akther <halimaakthermou004@gmail.com>
2026-05-18 23:56:49 +0600

    Fix server port in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: https://studynook-09.netlify.app,
+    port:5000,
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
     },
